38458e41e3f2bc0813f836fee70f8f3e4fc0db40
Ahmed Essam Azab <72676542+ahmedazab1235@users.noreply.github.com>
2025-08-12 21:42:59 +0300

    Update mmm.py

--- mmm.py
+++ mmm.py
@@ -1,28 +1,17 @@
 import streamlit as st
-from sklearn.datasets import load_iris
-from sklearn.linear_model import LogisticRegression
+from transformers import pipeline
 
-# Load data
-iris = load_iris()
-X = iris.data
-y = iris.target
-model = LogisticRegression(max_iter=200)
-model.fit(X, y)
 
-st.title("🌸 Iris Flower Prediction App")
+# Load the classification pipeline with the specified model
+pipe = pipeline("text-classification", model="tabularisai/multilingual-sentiment-analysis")
 
-st.write("Adjust the sliders to set flower features:")
+st.title("text classification App")
 
-# Feature sliders
-sepal_length = st.slider("Sepal length (cm)", float(X[:, 0].min()), float(X[:, 0].max()), float(X[:, 0].mean()))
-sepal_width = st.slider("Sepal width (cm)", float(X[:, 1].min()), float(X[:, 1].max()), float(X[:, 1].mean()))
-petal_length = st.slider("Petal length (cm)", float(X[:, 2].min()), float(X[:, 2].max()), float(X[:, 2].mean()))
-petal_width = st.slider("Petal width (cm)", float(X[:, 3].min()), float(X[:, 3].max()), float(X[:, 3].mean()))
+text = st.text_input("Enter your text", "I love this product! It's amazing and works perfectly.")
 
-# Make prediction
-input_features = [[sepal_length, sepal_width, petal_length, petal_width]]
-prediction = model.predict(input_features)
-predicted_class = iris.target_names[prediction[0]]
+result = pipe(text)
 
-st.subheader("🌼 Predicted Iris Class:")
-st.write(f"**{predicted_class}**")
+# Print the result
+st.write(result[0]['label'])
+st.write(result[0]['score'])
+
